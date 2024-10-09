7a0a575df3d124787a4ee92edb32e6b6ae9949a9
Askari <askarir64@gmail.com>
2024-10-09 22:53:30 +0530

    Added Signup and Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import SignIn from "./components/Sign-in/Signin"
+import { Routes, Route } from "react-router-dom"
+
+import SignIn from "./components/sign-in/SignIn"
+import SignUp from "./components/sign-up/SignUp"
 
 function App() {
 
   return (
-    <SignIn/>
+    <Routes>
+      <Route path="/" element={<SignIn/>} />
+      <Route path="/signup" element={<SignUp/>} />
+    </Routes>
   )
 }
 
